22762b21834d63cb00e52de14ff373b937f545f9
Aruna <myruala@gmail.com>
2020-06-17 16:11:31 +0300

    Updates

---
+++ precache-manifest.66efdc21dcdb6393cf66b8d6bf9b6c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34603e394a174a935c44fbe185e601d5",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "03fc8cfff8ffc08f494b",
+    "url": "/goit-react-hw-01-components/static/css/main.610d4eda.chunk.css"
+  },
+  {
+    "revision": "6ab8efbfb2a726fb4712",
+    "url": "/goit-react-hw-01-components/static/js/2.38458b62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.38458b62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03fc8cfff8ffc08f494b",
+    "url": "/goit-react-hw-01-components/static/js/main.f77ce91a.chunk.js"
+  },
+  {
+    "revision": "2c90fadf0dd2bf61ff2f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.1ea492af.js"
+  }
+]);
